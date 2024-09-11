e1bbaee62b451e685a2958f7beb12164c6cf6dc6
Emrul <emrulhossain49@gmail.com>
2024-09-11 23:42:56 +0600

    Create flipped_triangle.cpp

---
+++ flipped_triangle.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+void tri(int n)
+{
+    // number of spaces
+    int k = 2 * n - 2;
+
+    // Outer loop to handle number of rows
+    // n in this case
+    for (int i = n; i > 0; i--) {
+
+        // Inner loop to handle number spaces
+        // values changing acc. to requirement
+        for (int j = 0; j < n - i; j++)
+            cout << "  ";
+
+        // Decrementing k after each loop
+        k = k - 2;
+
+        // Inner loop to handle number of columns
+        // values changing acc. to outer loop
+        for (int j = 0; j < i; j++) {
+            // Printing stars
+            cout << "* ";
+        }
+
+        // Ending line after each row
+        cout << endl;
+    }
+}
+
+// Driver Code
+int main()
+{
+    int n = 5;
+
+    // Function Call
+    tri(n);
+    return 0;
+}
